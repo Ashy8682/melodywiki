4c92994b0e431eac4e2624da3c884d5daaa4bf26
Ashy Eset <62446238+Ashy8682@users.noreply.github.com>
2024-09-06 13:42:19 -0400

    Add files via upload

---
+++ quick.js
@@ -0,0 +1,12 @@
+
+function quick(url, elementId) {
+    fetch(url)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById(elementId).innerHTML = data;
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            document.getElementById(elementId).innerHTML = "Failed to load content.";
+        });
+}
